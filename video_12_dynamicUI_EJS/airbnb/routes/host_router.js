1c195655b45ae2189ddabeb6f1eee295aaf8cb20
vedant-s5 <vedant.singh0507@gmail.com>
2025-09-24 17:44:54 +0530

    video12: learn about dynamic ui using ejs

---
+++ video_12_dynamicUI_EJS/airbnb/routes/host_router.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+const express = require('express')
+const hostRouter = express.Router();
+
+const rootdir = require('../utils/path_util')
+
+
+hostRouter.get("/add-home",(req,res,next) => {
+  res.render('add-home', {pageTitle: 'add home to airbnb'})
+})
+
+const registeredHome = [];
+
+hostRouter.post("/add-home",(req,res,next) => {
+  console.log("home registration successful for: ",req.body, req.body.house);
+  registeredHome.push({house: req.body.house})
+  res.render('home-added', {pageTitle: 'home added successfully.'})
+})
+
+
+exports.hostRouter = hostRouter
+exports.registeredHome = registeredHome
